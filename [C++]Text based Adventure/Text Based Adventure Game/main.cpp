9b528b2551b0c9a8e915f5e9bb868068964ee619
Cpp4 <79259401+Priyansh5812@users.noreply.github.com>
2023-10-17 16:13:04 +0530

    Update

---
+++ [C++]Text based Adventure/Text Based Adventure Game/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{	
+	cout << "Once upon a time in the whimsical land of Mushroom Kingdom, there lived a humble plumber named Pete.\n Pete was known far and wide for his trademark red cap, blue overalls, and his boundless courage. \nHe lived in a quaint cottage nestled in the heart of Toadstool Village." << endl;
+	return 0;
+}
